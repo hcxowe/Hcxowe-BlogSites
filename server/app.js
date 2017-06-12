67c0e1bbf28b57e4fe2d7e49239ed18291306c9a
hcxowe <hcxowe@126.com>
2017-06-12 10:39:33 +0800

    update

--- server/app.js
+++ server/app.js
@@ -4,22 +4,22 @@ var history = require('connect-history-api-fallback');
 
 var app = express();
 
-const staticFileMiddleware = express.static('../front-webapp/dist/static');
-//app.use(express.static(path.join(__dirname, '../front-webapp/dist')));
+//const staticFileMiddleware = express.static(path.join(__dirname, '../front-webapp/dist'));
+app.use(express.static(path.join(__dirname, '../front-webapp/dist')));
 
-app.use(staticFileMiddleware);
+//app.use(staticFileMiddleware);
 
 app.use(history({
     disableDotRule: true,
     verbose: true
 }));
 
-app.use(staticFileMiddleware);
+//app.use(staticFileMiddleware);
 
 // app.get('/', function(req, res) {
-//      res.sendFile(path.join(__dirname, '../front-webapp/dist/index.html'));
+//     res.sendFile(path.join(__dirname, '../front-webapp/dist/index.html'));
 // })
 
-app.listen(3000, function() {
+app.listen(3100, function() {
     console.log('start server');
 });
